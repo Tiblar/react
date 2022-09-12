e0733789508bb572f3059364620a3bef9d47d8ba
odilitime <janesmith@airmail.cc>
2022-09-12 00:57:21 +0000

    disable iframing PDFs as newer chrome is forcing a download

--- src/components/desktop/layout/parts/social/post/media/pdf/PDF.js
+++ src/components/desktop/layout/parts/social/post/media/pdf/PDF.js
@@ -4,10 +4,12 @@ import React from "react";
 
 import postStyles from "../../../../../../../../css/components/post.css";
 
+// <iframe frameBorder={0} src={props.file.file.url}/>
+
 const PDF = (props) => {
     return (
         <div className={postStyles.pdf}>
-            <iframe frameBorder={0} src={props.file.file.url}/>
+
         </div>
     );
 };
